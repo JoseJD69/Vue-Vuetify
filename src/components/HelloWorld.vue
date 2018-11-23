d8f1edcefe41892712bea9f7831fc1db837f969f
JOSE <joseijd69@gmail.com>
2018-11-22 22:26:49 -0500

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,105 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+    <v-container>
+        <div class="mb-5">
+            <v-data-table
+                    :headers="headers"
+                    :items="desserts"
+                    class="elevation-1"
+
+            >
+                <template slot="items" slot-scope="props">
+                    <td>{{ props.item.name }}</td>
+                    <td class="text-xs-right">{{ props.item.calories }}</td>
+                    <td class="text-xs-right">{{ props.item.fat }}</td>
+                    <td class="text-xs-right">{{ props.item.carbs }}</td>
+                    <td class="text-xs-right">{{ props.item.protein }}</td>
+                    <td class="text-xs-right">{{ props.item.iron }}</td>
+                </template>
+            </v-data-table>
+        </div>
+        <div>
+            <v-data-table
+                    :headers="headersT"
+                    :items="UserList"
+                    class="elevation-1"
+            >
+                <template slot="items" slot-scope="props">
+                    <td>{{ props.item.name }}</td>
+                    <td class="text-xs-right">{{ props.item.email }}</td>
+                    <td class="text-xs-right">{{ props.item.phone }}</td>
+                </template>
+            </v-data-table>
+        </div>
+    </v-container>
+
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+    export default {
+        data() {
+            return {
+                urlUser: 'https://jsonplaceholder.typicode.com/users',
+                UserList: [],
+                headers: [
+                    {
+                        text: 'Dessert (100g serving)',
+                        align: 'left',
+                        sortable: false,
+                        value: 'name'
+                    },
+                    {text: 'Calories', value: 'calories'},
+                    {text: 'Fat (g)', value: 'fat'},
+                    {text: 'Carbs (g)', value: 'carbs'},
+                    {text: 'Protein (g)', value: 'protein'},
+                    {text: 'Iron (%)', value: 'iron'}
+                ],
+                headersT: [
+                    {
+                        text: 'Nombres',
+                        align: 'left',
+                        sortable: false,
+                        value: 'name'
+                    },
+                    {text: 'Email', value: 'email'},
+                    {text: 'Phone', value: 'phone'},
+                ],
+                desserts: [
+                    {
+                        value: false,
+                        name: 'Frozen Yogurt',
+                        calories: 159,
+                        fat: 6.0,
+                        carbs: 24,
+                        protein: 4.0,
+                        iron: '1%'
+                    },
+                    {
+                        value: false,
+                        name: 'Ice cream sandwich',
+                        calories: 237,
+                        fat: 9.0,
+                        carbs: 37,
+                        protein: 4.3,
+                        iron: '1%'
+                    }
+                ]
+            }
+        },
+        methods: {
+            getUser: function () {
+                this.axios.get(this.urlUser).then(response => {
+                    this.UserList = response.data
+                    console.log(this.UserList)
+                })
+
+            }
+        },
+        mounted: function () {
+            this.getUser()
+        }
+    }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 Vue.config.productionTip = false
-
+Vue.prototype.axios = axios
+Vue.use(axios, VueAxios)
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
